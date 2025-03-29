257ffabbb2f9d0767762bdb5ad583b09c65f4b0d
rohit saini <rohit1322saini@gmail.com>
2025-03-29 15:53:22 +0530

    idk

--- src/app/videos/[id]/page.jsx
+++ src/app/videos/[id]/page.jsx
@@ -1,6 +1,6 @@
 // import { uri } from "../../page.jsx";
 export default async function Videos({ params }) {
-    const uri = "https://www.stream.xxxvideoss.site"
+    const uri = "https://www.stream.xxxvideoss.site/api/stream/category?type=";
     let Category = "";
       const { id } = await params; 
       if(id === "1") Category = "Amateur";
@@ -14,14 +14,22 @@ export default async function Videos({ params }) {
         if(id === "9") Category = "Creampie";
 
       if (!params) return <div>Loading...</div>; // Handle case where params is not ready
-    //   const res = await fetch(`${uri}/api/stream/video?id=${id}`, { cache: "no-store" });
-
-  
+      const res = await fetch(`${uri}+${"porn"}`, { cache: "no-store" });
+      console.log(res)
+      if (!res.ok) {
+          throw new Error("Failed to fetch data");
+      }
       return (
           <main>
               <h1>Category : {Category}</h1>
-              <div className="footer">
-                <h3>Copyright 2025 rohit saini</h3>
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                  {/* {res.json().map((video) => (
+                      <div key={video.id} className="p-4 border rounded">
+                          <h2>{video.title}</h2>
+                          <img src={video.image} alt="X" />
+                          <p>{video.duration}</p>
+                      </div> */}
+                  {/* ))} */}
               </div>
           </main>
       );
