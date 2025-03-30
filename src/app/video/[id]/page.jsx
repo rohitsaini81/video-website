30e1c4a557df2d63102d6ab1eb5aeb619e123bc5
rohit saini <rohit1322saini@gmail.com>
2025-03-30 13:52:40 +0530

    tags

--- src/app/video/[id]/page.jsx
+++ src/app/video/[id]/page.jsx
@@ -1,5 +1,5 @@
 import { Video_Uri } from "@/app/layout";
-
+// import Video from 'next-video';
 // import { uri } from "../../page.jsx";
 export default async function VideoPreview({ params }) {
     const uri = "https://www.stream.xxxvideoss.site"
@@ -12,8 +12,8 @@ export default async function VideoPreview({ params }) {
     if (!video_data) return <div>Loading...</div>; // Handle case where video is not ready
     if (video_data.error) return <div>{video.error}</div>; // Handle error case
 
-    console.log(video_data.video);
-    console.log(Video_Uri);
+    console.log(video_data);
+
     let video =Video_Uri+ video_data.video;
     if (!video) return <div>Loading...\n Error !</div>; // Handle case where video is not ready
 
@@ -24,14 +24,24 @@ export default async function VideoPreview({ params }) {
                 Your browser does not support the video tag.
             </video>
             <br/>
+
+            <div className="flex">
+            {/* <Video src="https://pub-a919e0e7442047299d7072ac1b2ab5d0.r2.dev/temp_video.mp4"/> */}
+            </div>
             <div className="footer">
                 <h1>{video_data.title}</h1>
                 <hr/>
                 <p>Duration: {video_data.duration}</p>
                 <br/>
-                {video_data.title ==video_data.tag ?<></>:
-                <h3>tags : <span>{video_data.tag}</span></h3>
-                }
+                <h3>tags : <span> </span> 
+                {video_data.tagsList.map((tag, index) => (
+                    <span key={index} className="tag">
+                        {tag}
+                        {index !== video_data.tagsList.length - 1 && ", "}
+                    </span>
+                ))}
+                    </h3>
+                
             </div>
         </main>
     );
