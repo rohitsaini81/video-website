9c2a690fb8dda1335d5b32b3eb4c52cb9dc191ae
rohit saini <rohit1322saini@gmail.com>
2025-03-31 13:47:12 +0530

    video tag designed

---
+++ src/app/components/SimpleVideoPlayer.jsx
@@ -0,0 +1,157 @@
+import { useState, useEffect, useRef } from "react";
+import { AiOutlineSound } from "react-icons/ai";
+import { CiPlay1 } from "react-icons/ci";
+import { FaVolumeMute } from "react-icons/fa";
+import { CiPause1 } from "react-icons/ci";
+import { CiSettings } from "react-icons/ci";
+import { BsFullscreen } from "react-icons/bs";
+import { BsFullscreenExit } from "react-icons/bs";
+
+const VideoPlayer = ({ publicId }) => {
+  const video_ref = useRef(null);
+  const video_player_ref = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isMuted, setIsMuted] = useState(false);
+  const [volume, setVolume] = useState(1);
+  const [progress, setProgress] = useState(0);
+  const [isFullScreen, setIsFullScreen] = useState(false);
+  const [buffering, setBuffering] = useState(false);
+
+
+
+  useEffect(() => {
+    const handleKeyPress = (e) => {
+      if (e.code === "Space") {
+        e.previntDefault(); 
+        handleTogglePlay();
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyPress);
+
+    // Cleanup function to remove event listener when component unmounts
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, []);
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      console.log("Search triggered:", e.target.value);
+    }
+  }
+
+  const handleTogglePlay = () => {
+    if (video_ref.current) {
+      if (video_ref.current.paused) {
+        video_ref.current.play();
+        setIsPlaying(true);
+      } else {
+        video_ref.current.pause();
+        setIsPlaying(false);
+      }
+    }
+  };
+
+  const HandleControl = useRef(null);
+  const HandleFullScreen = () => {
+    if (document.fullscreenElement) {
+      HandleControl.current.classList.remove("controls-full-screen");
+      document.exitFullscreen();
+      setIsFullScreen(false);
+    } else {
+      video_player_ref.current.requestFullscreen();
+      HandleControl.current.classList.add("controls-full-screen");
+      setIsFullScreen(true);
+    }
+  };
+
+  const handleVolumeChange = (e) => {
+    let volume_value = parseFloat(e.target.value); // Ensure it's a number
+    volume_value = Math.min(1, Math.max(0, volume_value)); // Clamp between 0 and 1
+
+    setVolume(volume_value);
+
+    if (video_ref.current) {
+      if (volume_value === 0) {
+        setIsMuted(true);
+        video_ref.current.muted = true;
+      } else {
+        setIsMuted(false);
+        video_ref.current.muted = false;
+      }
+      video_ref.current.volume = volume_value;
+    }
+  };
+
+  const handleProgressChange = (e) => {
+    const progress = parseFloat(e.target.value); // Ensure it's a float
+    setProgress(progress);
+    if (video_ref.current && video_ref.current.duration) {
+      video_ref.current.currentTime = video_ref.current.duration * progress;
+    }
+  };
+  const toggleMute = () => {
+    if (video_ref.current) {
+      video_ref.current.muted = !isMuted; // Mute/unmute the video
+      setIsMuted(!isMuted); // Update state
+    }
+  };
+
+  return (
+    <div ref={video_player_ref} className="video-player">
+      <video ref={video_ref}>
+        <source src={publicId} />
+      </video>
+      <div
+        ref={HandleControl}
+        className="controls bg-amber-200 h-10 flex items-center justify-between p-2"
+      >
+        <button
+          onClick={handleTogglePlay}
+          className="m-1 p-2 bg-gray-700 hover:bg-gray-600 rounded play-button"
+        >
+          {isPlaying ? <CiPause1 /> : <CiPlay1 />}
+        </button>
+        <button
+          onClick={HandleFullScreen}
+          className="m-1 p-2 bg-gray-700 hover:bg-gray-600 rounded fullscreen-button"
+        >
+          {isFullScreen ? <BsFullscreenExit /> : <BsFullscreen />}
+        </button>
+        <button
+          className="m-1 p-2 bg-gray-700 hover:bg-gray-600 rounded"
+          htmlFor="volume"
+        >
+          {!isMuted?<AiOutlineSound onClick={toggleMute}/>:<FaVolumeMute onClick={toggleMute}/>}
+        </button>
+        <input
+          name="volume"
+          type="range"
+          min="0"
+          max="1"
+          step="0.01"
+          value={volume}
+          onChange={handleVolumeChange}
+          className="m-1 w-1/8 h-1 bg-gray-500 cursor-pointer"
+        />
+
+        <input
+          name="progress"
+          type="range"
+          min="0"
+          max="1"
+          step="0.01"
+          value={progress}
+          onChange={handleProgressChange}
+          onWaiting={() => setBuffering(true)} // Fires when buffering starts
+          onPlaying={() => setBuffering(false)}
+          className="m-1 w-1/2 h-1 bg-gray-500 cursor-pointer"
+        />
+        <button className="p-2 bg-gray-700 hover:bg-gray-600 rounded quality-button">
+          <CiSettings />
+        </button>
+      </div>
+    </div>
+  );
+};
+export default VideoPlayer;
--- src/app/watch/[slug]/page.jsx
+++ src/app/watch/[slug]/page.jsx
@@ -3,87 +3,68 @@
 import { useState, useEffect } from "react";
 import { useParams } from "next/navigation";
 import { Video_Uri } from "@/app/layout";
-import ReactPlayer from "react-player";
-
+import VideoPlayer from "@/app/components/SimpleVideoPlayer"; // Import your custom video player component
 
 const Watch = () => {
-    const { slug } = useParams() || {}; // Ensure it doesn't crash
+  const { slug } = useParams() || {}; // Ensure it doesn't crash
 
-    const new_query = slug ? decodeURIComponent(slug) : ""; // Handle undefined
-    console.log("Query:", slug, "Decoded:", new_query);
-    const [videos, setVideos] = useState([]);
-    const [video_source, setVideo_source] = useState("");
-    useEffect(() => {
+  const new_query = slug ? decodeURIComponent(slug) : ""; // Handle undefined
+  console.log("Query:", slug, "Decoded:", new_query);
+  const [videos, setVideos] = useState([]);
+  const [video_source, setVideo_source] = useState("");
+  const temp_video =
+    "https://pub-a919e0e7442047299d7072ac1b2ab5d0.r2.dev/video.mp4";
+  useEffect(() => {
+    const fetchVideos = async () => {
+      if (!slug) return;
+      try {
+        const uri = `/api/proxy?query=${encodeURIComponent(new_query)}`;
+        console.log("Fetching from:", uri);
+        const response = await fetch(uri, { cache: "no-store" });
+        const data = await response.json();
+        setVideos(data);
+        let temp = Video_Uri + data[0].video;
+        setVideo_source(temp);
+        // console.log(data[0]);
+      } catch (error) {
+        console.error("Error fetching videos:", error);
+      }
+    };
 
-        const fetchVideos = async () => {
-            if (!slug) return;
-            try {
-                const uri = `/api/proxy?query=${encodeURIComponent(new_query)}`;
-                console.log("Fetching from:", uri);
-                const response = await fetch(uri, { cache: "no-store" });
-                const data = await response.json();
-                setVideos(data);
-                let temp = Video_Uri + data[0].video;
-                setVideo_source(temp);
-                // console.log(data[0]);
-            } catch (error) {
-                console.error("Error fetching videos:", error);
-            }
-        };
+    fetchVideos();
+  }, [slug]); // Re-fetch when the query changes
 
-        fetchVideos();
-    }, [slug]); // Re-fetch when the query changes
-
-    return (
-        <div className="flex flex-col items-center">
-            <div className="flex items-center">
-                <div className="flex bg-white border-2 border-pink-500 shadow-lg rounded-lg overflow-hidden p-2">
-                    <p className="text-red-500 font-bold">Watch HD:</p>
-                    <span className="ml-2 text-gray-700">{new_query}</span>
-                </div>
-            </div>
-            <br />
-            <hr />
-            <div>
-
-                <ul>
-                    <div style={{ display: "flex", gap: "20px", flexWrap: "wrap" }}>
-                        {
-                            videos.length > 0 ?
-                                <div className="flex">
-
-                                    <video
-                                        src={video_source}
-                                        className="video"
-                                        height="320" width="400" 
-                                        controls/>
-                                    {/* <ReactPlayer /> */}
-
-
-
-
-                                    <li className="mt-2 p-2 border-b">
-                                        <img src={videos[0].image} alt={videos[0].title} />
-                                        <p>{videos[0].title}</p>
-                                        <p>{videos[0].duration}</p>
-                                    </li>
-                                </div>
-                                :
-                                <div>
-                                    <p className="text-gray-500">No Video found.</p>
-                                </div>
-
-                        }
-                    </div>
-                </ul>
-
-
-
-
-
-            </div>
+  return (
+    <div className="flex flex-col items-center">
+      <div className="flex items-center">
+        <div className="flex bg-white border-2 border-pink-500 shadow-lg rounded-lg overflow-hidden p-2">
+          <p className="text-red-500 font-bold">Watch HD:</p>
+          <span className="ml-2 text-gray-700">{"new_query"}</span>
         </div>
-    );
+      </div>
+      <br />
+      <hr />
+      <div>
+        <ul>
+          <div style={{ display: "flex", gap: "20px", flexWrap: "wrap" }}>
+            {videos.length > 0 ? (
+              <div className="flex">
+                <VideoPlayer publicId={video_source} />
+                <li className="mt-2 p-2 border-b">
+                  {/* <p>{videos[0].title}</p> */}
+                  <p>{videos[0].duration}</p>
+                </li>
+              </div>
+            ) : (
+              <div>
+                <p className="text-gray-500">No Video found.</p>
+              </div>
+            )}
+          </div>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Watch;
